676c699df93165c630a52b972cc6c7b9a47184f3
av <vihrogonov@protonmail.com>
2020-12-17 00:35:51 +0000

    Checkbox Functionality

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+  // change the theme of website
+  document.body.classList.toggle('dark');
+  document.body.classList.toggle('content-publish');
+});
+
 /* sweetScroll load */
 document.addEventListener(
   'DOMContentLoaded',
